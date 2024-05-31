f385595c9162e8bca9a1f098f486e2ec9d08b61c
Arnav Jha <91931013+left01205@users.noreply.github.com>
2024-05-31 22:45:05 +0530

    Update operations_on_built_in_datatypes.py

--- UNIT_1/operations_on_built_in_datatypes.py
+++ UNIT_1/operations_on_built_in_datatypes.py
@@ -20,6 +20,25 @@ def Tuples():
   l2=(1.0,2.5,3.6,54.3)
   print("INDEXING: ",l1[0],l2[1]) # output -> 1 2.5
   print("SLICING : ",l1[0:3])   # output -> (1,2,3)
-  print("CONCATENATION : ",l1+lb) # output -> (1,2,3,4,5,6,1.0,2.5,3.6,54.3)
+  print("CONCATENATION : ",l1+l2) # output -> (1,2,3,4,5,6,1.0,2.5,3.6,54.3)
   print("REPETITION : ",l1*2)  # output -> (1,2,3,4,5,6,1,2,3,4,5,6)
+def Dictionary():
+  d1={'A':10,'B':20,'C':30}
+  d2={'D':40,'E':50,'F':60}
+  print("INDEXING: ",d1['B'],d2['F']) # output -> 20 60
+  '''
+  DICTIONARY DOESN'T SUPPORT SLICING, CONCATENATION  OR REPETITION
+  IT DOES HAVE update() METHOD.
+  '''
+  def Sets():
+    s1={1,2,3,4,5}
+    s2={6,7,8,9,10}
+    '''
+    DOESN'T SUPPORT EITHER OF 'EM
+    '''
 
+Strings()
+Lists()
+Tuples()
+Dictionary()
+Sets()
