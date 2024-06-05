87715cf210e8e52efb3001a1d52e6d8c6f954a1d
left01205 <student4.ai@csvtu.ac.in>
2024-06-05 22:32:25 +0530

    Adding File

---
+++ UNIT_1/Math_Ran.py
@@ -0,0 +1,20 @@
+import math
+import random
+
+x = 4.3
+y = 2.7
+
+
+sqrt_x = math.sqrt(x)
+print(f"The square root of {x} is {sqrt_x}")
+
+
+ceil_y = math.ceil(y)
+floor_y = math.floor(y)
+print(f"The ceiling of {y} is {ceil_y}")
+print(f"The floor of {y} is {floor_y}")
+
+random_int = random.randint(1, 10)
+print(f"Random integer between 1 and 10: {random_int}")
+random_float = random.random()
+print(f"Random floating-point number between 0 and 1: {random_float}")
