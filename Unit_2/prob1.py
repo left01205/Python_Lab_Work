defc1068101ad7c8932ad9f6e85f75ffa8dfa181
left01205 <student4.ai@csvtu.ac.in>
2024-06-05 22:54:37 +0530

    Adding Problem1 solution , pack1.py is the package file used.

---
+++ Unit_2/prob1.py
@@ -0,0 +1,9 @@
+import pack1 as p
+#Imported Package 'Pack1' with alias name 'p'
+num1=10
+num2=20
+print("Addition of two numbers is:",p.add(num1,num2))
+print("Subtraction of two numbers is:",p.subtract(num1,num2))
+print("Multiplication of two numbers is:",p.multiply(num1,num2))
+print("Division of two numbers is:",p.divide(num1,num2))
+print("Power of two numbers is:",p.power(num1,num2))
