2312aed8acea68c6cb0bf54c186d5f374546c0f6
left01205 <student4.ai@csvtu.ac.in>
2024-06-05 22:59:11 +0530

    Adding File Manipulation code

---
+++ Unit_2/prob2.py
@@ -0,0 +1,30 @@
+file1 = open("file1.txt", "r")
+
+
+content = file1.read()
+
+
+file1.close()
+
+
+file2 = open("file2.txt", "w")
+
+
+file2.write(content)
+
+
+file2.close()
+
+
+file3 = open("file3.txt", "a")
+
+
+file3.write("This is some additional content.")
+
+file3.close()
+
+
+with open("file1.txt", "r") as source_file:
+    with open("file4.txt", "w") as destination_file:
+        for line in source_file:
+            destination_file.write(line)
