59b580045cf87d8ace24ac89a0ad3ac26542b6e6
left01205 <student4.ai@csvtu.ac.in>
2024-06-06 17:55:39 +0530

    Creating numpy arrays by defined data struct and by numpy methods

---
+++ Unit_3/Problem1.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+ld = [1, 2, 3, 4, 5]
+afl = np.array(ld)
+
+td = (6, 7, 8, 9, 10)
+aft = np.array(td)
+
+
+zeros_array = np.zeros(5)  
+ones_array = np.ones((3, 3))
+empty_array = np.empty(4)  
+
+random_array = np.random.rand(5)  
+random_int_array = np.random.randint(1, 10, size=(2, 3))  
+
